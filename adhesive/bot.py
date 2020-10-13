37a857b0676ced69c794d3910cd9ea6bab6dbe21
io mintz <gie9ohbeixah@paperboats.net>
2020-10-13 13:02:49 +0000

    clean up the bot intro message a bit

--- adhesive/bot.py
+++ adhesive/bot.py
@@ -3,9 +3,9 @@
 import anyio
 from signalstickers_client import StickersClient as SignalStickersClient
 
-INTRO = """
+INTRO = """\
 Hi there! I'm a simple bot that converts Telegram stickers to Signal stickers and back.
-To begin, send me a link to either a sticker pack. Here's what a telegram sticker pack looks like:
+To begin, send me a link to either kind of sticker pack. Here's what a Telegram sticker pack looks like:
 `https://t.me/addstickers/animals`
 And here's a Signal sticker pack:
 `https://signal.art/addstickers/#pack_id=9acc9e8aba563d26a4994e69263e3b25&pack_key=5a6dff3948c28efb9b7aaf93ecc375c69fc316e78077ed26867a14d10a0f6a12`
