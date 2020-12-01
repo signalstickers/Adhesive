ce8b024c8d3d2db4b03d8e8d73c62636dad574d9
io mintz <gie9ohbeixah@paperboats.net>
2020-12-01 22:44:28 +0000

    schema: add converted_at for timed deletion

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
+-- This table stores packs converted from Telegram to Signal.
+-- It does not store the reverse, because our Telegram sticker pack URLs include the signal pack_id
+-- and that's used instead of this table.
 CREATE TABLE packs (
 	tg_hash INTEGER PRIMARY KEY,
 	signal_pack_id BLOB NOT NULL,
-	signal_pack_key BLOB NOT NULL
+	signal_pack_key BLOB NOT NULL,
+	-- for timed deletion
+	-- UTC seconds since 1970 without leap seconds
+	converted_at INTEGER NOT NULL DEFAULT cast(strftime('%s', 'now') AS INT)
 );
